36531d828bf416d7f51ab4e3df4f362af8d51aa9
diogo <diogo.lima.flooxai@gmail.com>
2025-03-24 08:44:48 -0300

    refactor: simplifying code with Send

---
+++ utils/error.js
@@ -0,0 +1,10 @@
+const send = require("send");
+
+module.exports = {
+  send: (err, req, res, code = 400)=>{
+    console.log(`error:${err}`);
+          res.status(400).json({
+            error:err
+          });
+  }
+}
